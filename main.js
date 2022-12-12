68b37a0316e966fa2f3d49925a086856a5551284
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-12 15:09:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-// Make sure SW are supported//
-if ('serviceWorker' in navigator){
-    window.addEventListener('load', ()=> {
-        navigator.serviceWorker
-        .register('the_cached_site.js') 
-        .then(reg => console.log('Service Worker: Registered'))
-        .catch(err => console.log('Service Worker: Error:${err}'))
-      });
-}
+let cacheVersion = 1
+let cacheName = "web-workr-cache-"+cacheVersion
+const pageToSave = "offline.html"
+
+// Installing service worker
+this.addEventListener('install', event => {
+    console.log("Installing service worker");
+  
+    event.waitUntil(caches.open(cacheName)
+    .then((openCache) => {
+        return openCache.add(pageToSave)
+    })
+    .catch(err => console.log(err)))
+})
