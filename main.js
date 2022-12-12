50dd2a201727a5b871fae02bd6891ead304d43d5
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-12 16:03:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Registering. Should be in the JS file contained in the html files.
 window.addEventListener('load', () => {
     navigator.serviceWorker
-    .register('the_cached_site.js')
+    .register('../the_cached_site.js')
     .then(_ => console.log('Registered service worker'))
     .catch(e => console.log('Error registering: ',e));
 });
