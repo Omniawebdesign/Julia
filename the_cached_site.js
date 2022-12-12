569eb5cb26fbcc597436547a4d93693256bc90f8
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-12 16:19:13 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,52 +1,14 @@
- const cacheName = 'v1';
+let cacheVersion = 1
+let cacheName = "web-workr-cache-"+cacheVersion
+const pageToSave = "index.html"
 
- const cacheAssets = [
-    'index.html',
-    'juliacontact.html',
-    
-    '/js/main.js'
- ];
-    
- 
- 
- // Call Install Event
-self.addEventListener('install', e =>{
-    console.log('Service Worker: Installed');
-
-    e.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-           console.log('Service Worker : Caching Files');
-           cache.addAll(cacheAssets);
-        })
-            .then(() => self.skipWaiting())
-);
-});
-
-// Call Activate Event
-self.addEventListener('activate', e =>{
-    console.log('Service Worker: Activated');
-    // Remove unwanted caches
-    e.waitUntil(
-        cache.keys().then(cacheNames = =>{
-            return Promise.all(
-                cacheNames.map(cache =>{
-                    if(cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-}); 
-
-// Call Fetch Event
-self.addEventListener('fetch', e =>{
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request)))
-    
-});
-
+// Installing service worker
+this.addEventListener('install', event => {
+    console.log("Installing service worker");
+  
+    event.waitUntil(caches.open(cacheName)
+    .then((openCache) => {
+        return openCache.add(pageToSave)
+    })
+    .catch(err => console.log(err)))
+})
