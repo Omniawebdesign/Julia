2cc6b1f2c25ce94112f06413d3e08f378384d81f
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-12 17:06:17 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,18 +1,23 @@
-let cacheVersion = 1
-let cacheName = "web-workr-cache-"+cacheVersion
-const pageToSave = ['index.html','stylesju lia.css','core-styles.css'];
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-// Installing service worker
-this.addEventListener('install', event => {
-    console.log("Installing service worker");
-  
-    event.waitUntil(caches.open(cacheName)
-    .then((openCache) => {
-        return openCache.add(pageToSave)
-    })
-    .catch(err => console.log(err)))
-})
-
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/app.js",
+      "/image-list.js",
+      "/star-wars-logo.jpg",
+      "/gallery/bountyHunters.jpg",
+      "/gallery/myLittleVader.jpg",
+      "/gallery/snowTroopers.jpg",
+    ])
+  );
+});
 
 
 
