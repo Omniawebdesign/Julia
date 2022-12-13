a36f14373d608980cc4f1b0480925418af9cd7e1
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-13 11:02:18 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,61 +1,39 @@
- const cacheName = 'v1';
+ let cacheVersion = 1
+let cacheName = "web-workr-cache-"+cacheVersion
+const pageToSave = ['index.html','stylesju lia.css','core-styles.css'];
 
- const cacheAssets = [
-    'index.html',
-    'about.html',
-    '/css/style.css',
-    '/js/main.js'
- ];
-    
- 
- 
- // Call Install Event
-self.addEventListener('install', e =>{
-    console.log('Service Worker: Installed');
+// Installing service worker
+this.addEventListener('install', event => {
+    console.log("Installing service worker");
+  
+    event.waitUntil(caches.open(cacheName)
+    .then((openCache) => {
+        return openCache.add(pageToSave)
+    })
+    .catch(err => console.log(err)))
+})
 
-    e.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-           console.log('Service Worker : Caching Files');
-           cache.addAll(cacheAssets);
-        })
-            .then(() => self.skipWaiting())
-);
-});
 
-// Call Activate Event
-self.addEventListener('activate', e =>{
-    console.log('Service Worker: Activated');
-    // Remove unwanted caches
-    e.waitUntil(
-        cache.keys().then(cacheNames = =>{
-            return Promise.all(
-                cacheNames.map(cache =>{
-                    if(cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-}); 
 
-// Call Fetch Event
-self.addEventListener('fetch', e =>{
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request)))
-    
+
+
+self.addEventListener('activate', e => {
+console.log('Activation!');
 });
 
 
 
 
 
-
-
-
-
-
+// Fetching resource
+this.addEventListener('fetch', event => {
+    console.log("Fetching with service worker");
+    if(event.request.mode === 'navigate'){
+        event.respondWith(
+            fetch(event.request.url)
+            .catch(_ => {
+                return caches.match(pageToSave)
+            })
+        )
+    }
+})
