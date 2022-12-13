3b0ad4f8b6a449230981dda9508a19a610abfeba
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-13 10:59:39 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,29 +1,53 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
-};
+ const cacheName = 'v1';
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache[
-      "/",
-      "index.html",
-      "/style.css",
-      "/app.js",
-      "/image-list.js",
-      "/star-wars-logo.jpg",
-      "/gallery/bountyHunters.jpg",
-      "/gallery/myLittleVader.jpg",
-      "/gallery/snowTroopers.jpg",
-    ]
-  );
+ const cacheAssets = [
+    'index.html',
+    'about.html',
+    '/css/style.css',
+    '/js/main.js'
+ ];
+    
+ 
+ 
+ // Call Install Event
+self.addEventListener('install', e =>{
+    console.log('Service Worker: Installed');
+
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+           console.log('Service Worker : Caching Files');
+           cache.addAll(cacheAssets);
+        })
+            .then(() => self.skipWaiting())
+);
 });
 
+// Call Activate Event
+self.addEventListener('activate', e =>{
+    console.log('Service Worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+        cache.keys().then(cacheNames = =>{
+            return Promise.all(
+                cacheNames.map(cache =>{
+                    if(cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+}); 
 
-
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request));
+// Call Fetch Event
+self.addEventListener('fetch', e =>{
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request)))
+    
 });
 
 
