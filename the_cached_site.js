86e4f4a49e6d2740ba57155133951558ab99a622
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-13 12:52:07 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,6 +1,6 @@
 let cacheVersion = 1
 let cacheName = "web-workr-cache-"+cacheVersion
-const pageToSave = ['index.html','stylesjulia.css','core-styles.css'];
+const pageToSave = ['index.html','stylesjulia.css','core-styles.css', 'juliacontact.html'];
 
 // Installing service worker
 this.addEventListener('install', event => {
