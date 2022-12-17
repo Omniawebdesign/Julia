558d9070a7411fcc1cb80be40b138cf37e64c46d
Omniawebdesign <110055955+Omniawebdesign@users.noreply.github.com>
2022-12-17 09:12:28 +0100

    Update the_cached_site.js

--- the_cached_site.js
+++ the_cached_site.js
@@ -1,2 +1,42 @@
+let cacheVersion = 1
+let cacheName = "web-workr-cache-"+cacheVersion
+const pageToSave = ['juliacontact.html','stylesjulia.css','core-styles.css', 'index.html'];
+
+// Installing service worker
+this.addEventListener('install', event => {
+    console.log("Installing service worker");
+  
+    event.waitUntil(caches.open(cacheName)
+    .then((openCache) => {
+        return openCache.addAll(pageToSave)
+    })
+    .catch(err => console.log(err)))
+})
+
+     
+
+
+     
+self.addEventListener('activate', e => {
+console.log('Activation!');
+});
+
+ 
+
+
+
+// Fetching resource
+this.addEventListener('fetch', event => {
+    console.log("Fetching with service worker");
+    if(event.request.mode === 'navigate'){
+        event.respondWith(
+            fetch(event.request.url)
+            .catch(_ => {
+                return caches.match(pageToSave)
+            })
+        )
+    }
+})
+
 
 
